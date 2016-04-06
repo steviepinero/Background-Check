50847987e1ae20ef2a63f39b28e999d45941d1dd
Stevie <steviepinero@gmail.com>
2016-04-06 15:05:59 -0400

    logic for another way to get data from api. not sure which to use

--- app/controllers/criminal_check_controller.rb
+++ app/controllers/criminal_check_controller.rb
@@ -20,4 +20,50 @@ class CriminalCheckController < ApplicationController
     @result = HTTParty.post(@url, :body => {:subject => 'This is the screen name', :issue_type => 'Application Problem', :status => 'Open', :priority => 'Normal', :description => 'This is the description for the problem'})
     end
 
+
+#     curl -d
+# '{
+#   "credentials": {
+#       "account_id": "123456",
+#       "api_key": "EGi5e7caJioM49lOXMANJZCoum"
+#   },
+#   "product": "federal_criminal",
+#   "data": {
+#       "FirstName": "Jack",
+#       "LastName": "Graham",
+#       "MiddleName": "A",
+#       "Limit": 25
+#   }
+# }' https://api.imsasllc.com/v3/
+
+
+    def curl
+      require 'uri'
+      require 'net/http'
+
+      url = URI("https://api.imsasllc.com/v3/")
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+
+
+      request = Net::HTTP::Post.new(url)
+      credentials = {"account_id" => "127999",
+                      "api_key" => "oVuiZ6gfhwT41MoBJDMUyS6y4B" }
+
+      product = "federal_criminal"
+
+      data = {
+              "FirstName" => "#{firstName}",
+              "lastName" => "#{lastName}",
+              "middleName" => "#{middleName}",
+              "limit" => "#{limit}"
+      }
+
+      response = http.request(request)
+      puts response.read_body
+    end
+
 end
