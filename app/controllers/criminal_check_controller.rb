6cfc87a7002283903fe5885caac8547eef52e690
Stevie <steviepinero@gmail.com>
2016-04-06 16:20:09 -0400

    post request logic

--- app/controllers/criminal_check_controller.rb
+++ app/controllers/criminal_check_controller.rb
@@ -1,23 +1,16 @@
 class CriminalCheckController < ApplicationController
-    def checker(data)
-    @account_id = '127999'
-    @api_key = 'oVuiZ6gfhwT41MoBJDMUyS6y4B'
-    @product = 'federal_criminal'
-    @url = 'https://api.imsasllc.com/v3/ '
-    @data = data[
-      @firstName = firstName,
-      @lastName = lastName,
-      @middleName = middleName,
-      @limit = limit
-    ]
+    def curl_request
+@http = Curl.post("https://api.imsasllc.com/v3/", { :credentials {:account_id => "12799",
+                                                        :api_key => "oVuiZ6gfhwT41MoBJDMUyS6y4B"},
+                                                        :product => "federal_criminal",
+                                                        :data {
+                                                          :firstName => "jack",
+                                                          :lastName  => "graham",
+                                                          :middleName => "A",
+                                                          :limit     => 25
+                                                        },
+                                                        })
 
-    response =  HTTParty.get(@url)
-    @parsed = JSON.parse(response)
-
-
-    #submit and get a result
-
-    @result = HTTParty.post(@url, :body => {:subject => 'This is the screen name', :issue_type => 'Application Problem', :status => 'Open', :priority => 'Normal', :description => 'This is the description for the problem'})
     end
 
 
