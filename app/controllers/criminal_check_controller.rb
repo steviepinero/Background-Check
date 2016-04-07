50cbfc5820959115baf4c46ec30c0e2aedb40ba0
Stevie <steviepinero@gmail.com>
2016-04-07 15:10:34 -0400

    api call working in ruby file

--- app/controllers/criminal_check_controller.rb
+++ app/controllers/criminal_check_controller.rb
@@ -1,6 +1,6 @@
 class CriminalCheckController < ApplicationController
-    def curl_request
-@http = Curl.post("https://api.imsasllc.com/v3/", { :credentials {:account_id => "12799",
+    def initialize
+@http = HTTParty.post("https://api.imsasllc.com/v3/", { :credentials {:account_id => "12799",
                                                         :api_key => "oVuiZ6gfhwT41MoBJDMUyS6y4B"},
                                                         :product => "federal_criminal",
                                                         :data {
@@ -14,49 +14,47 @@ class CriminalCheckController < ApplicationController
     end
 
 
-#     curl -d
-# '{
-#   "credentials": {
-#       "account_id": "123456",
-#       "api_key": "EGi5e7caJioM49lOXMANJZCoum"
-#   },
-#   "product": "federal_criminal",
-#   "data": {
-#       "FirstName": "Jack",
-#       "LastName": "Graham",
-#       "MiddleName": "A",
-#       "Limit": 25
-#   }
-# }' https://api.imsasllc.com/v3/
-
-
-    def curl
-      require 'uri'
-      require 'net/http'
-
-      url = URI("https://api.imsasllc.com/v3/")
-
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-
-
-      request = Net::HTTP::Post.new(url)
-      credentials = {"account_id" => "127999",
-                      "api_key" => "oVuiZ6gfhwT41MoBJDMUyS6y4B" }
-
-      product = "federal_criminal"
-
-      data = {
-              "FirstName" => "#{firstName}",
-              "lastName" => "#{lastName}",
-              "middleName" => "#{middleName}",
-              "limit" => "#{limit}"
-      }
-
-      response = http.request(request)
-      puts response.read_body
+    def backgroundchecker
+        require 'httparty'
+        require 'JSON'
+        ​
+        options = {
+          :credentials => {
+            :account_id => "12799",
+            :api_key => "oVuiZ6gfhwT41MoBJDMUyS6y4B"
+          },
+          :product => "criminal_database",
+          :data => {
+          :FirstName => "Willie",
+          :LastName => "Hicks",
+          :MiddleName => "A",
+          :Address => "3301 BELMONT BLVD",
+          :City => "Chicago",
+          :State => "IL",
+          :Zip => "34232",
+          :County => "Cook",
+          :DOB => "1964-12-16",
+          :AgeMin => 60,
+          :AgeMax => 70,
+          :Limit => 5,
+          :ExactMatch => "no"
+}
+        }
+        ​
+        opt = JSON.generate(options)
+        ​
+        url = 'https://api.imsasllc.com/v3/'
+        ​
+        response = HTTParty.post(url, body: opt, :headers => {'Content-Type' => 'application/json' })
+        ​
+        parsed = JSON.parse(response.body)
+        ​
+        p parsed
     end
+
+
+def check_params
+    params.require(:user).permit(:firstname, :middleName, :lastName, :address, :city, :zip, :county, :dob, :limit, :exactMatch)
+end
 
 end
---
+++ db/migrate/20160407183344_backgroundcheck.rb
@@ -0,0 +1,4 @@
+class Backgroundcheck < ActiveRecord::Migration
+  def change
+  end
+end
