747aaa927aa75de454f7ba2359dacc5fa51ff819
Stevie <steviepinero@gmail.com>
2016-04-08 15:30:52 -0400

    update readme

--- app/controllers/record_checks_controller.rb
+++ app/controllers/record_checks_controller.rb
@@ -38,10 +38,10 @@ class RecordChecksController < ApplicationController
     p @parsed
     p @saved
     end
-
+    #Generates aproval code and saves it to a variable TODO: include this variable in the DB
   def generate_approval_code(size = 9)
   charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
-  (0...size).map{ charset.to_a[rand(charset.size)] }.join
+  @code = (0...size).map{ charset.to_a[rand(charset.size)] }.join
   end
 
   # GET /record_checks
