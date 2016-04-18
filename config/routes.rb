50c125a31bfc390c506b34b1464779997d2cd4db
Stevie <steviepinero@gmail.com>
2016-04-17 22:04:31 -0400

    record check routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get  'record_checks/create' => 'record_checks#new'
+  post 'record_checks/create' => 'record_checks#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
