e577c76409e8da2d8b36f8eb2f4fc0ee20ebd7ba
Stevie <steviepinero@gmail.com>
2016-04-23 17:26:21 -0400

    style application page

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem 'animate-rails'
 
 gem 'wow-rails'
 
+
+
 # Use ActiveModel has_secure_password
  gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/record_checks_controller.rb
+++ app/controllers/record_checks_controller.rb
@@ -58,12 +58,12 @@ p @parsed
 
   end
 
-  def generate_approval_code(size = 9)
-  charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
-  (0...size).map{ charset.to_a[rand(charset.size)] }.join
-
-
-  end
+  # def generate_approval_code(size = 9)
+  # charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
+  # (0...size).map{ charset.to_a[rand(charset.size)] }.join
+  #
+  #
+  # end
 
 def approval
   @parsed.extend Hashie::Extensions::DeepFind
@@ -75,8 +75,9 @@ def approval
      else
   p @deeper
    @decision = "approved"
-   @approval_code = generate_approval_code
+   @approval_code = SecureRandom.hex(6)
    p @approval_code
+
      end
 end
 
