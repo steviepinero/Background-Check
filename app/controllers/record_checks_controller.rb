4d5a07f693dad24b1c79117d90f71c8ec3b618b1
Stevie <steviepinero@gmail.com>
2016-04-23 18:53:51 -0400

    login path, need to fix bug

--- app/controllers/record_checks_controller.rb
+++ app/controllers/record_checks_controller.rb
@@ -1,5 +1,8 @@
 class RecordChecksController < ApplicationController
 attr_accessor :first_name, :last_name, :middle_name, :address, :city, :state, :zip, :county, :dob, :approval_code
+before_action :require_user
+
+
   def new
   @record_check = RecordCheck.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,13 @@ Rails.application.routes.draw do
   get  'record_checks' => 'record_checks#new'
   post 'record_checks/create' => 'record_checks#create'
 
+  get 'signup' => 'users#new'
+   resources :users
+
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
