374db36a723a98e4c140e2d5047d137001d99025
Stevie <steviepinero@gmail.com>
2016-04-25 19:50:25 -0400

    edit root path

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'record_checks#new'
   get  'record_checks' => 'record_checks#new'
   post 'record_checks/create' => 'record_checks#create'
 
